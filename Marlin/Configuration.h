c2ad8a75876a9d79c6ea1347ac784f8c52cef7e6
profesor79 <git@profesor79.pl>
2018-08-01 00:25:28 +0100

    settings

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -704,7 +704,8 @@
 /**
  * The BLTouch probe uses a Hall effect sensor and emulates a servo.
  */
-//#define BLTOUCH
+ 
+#define BLTOUCH
 #if ENABLED(BLTOUCH)
   //#define BLTOUCH_DELAY 375   // (ms) Enable and increase if needed
 #endif
@@ -827,7 +828,7 @@
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
 #define INVERT_X_DIR false
 #define INVERT_Y_DIR false
-#define INVERT_Z_DIR true
+#define INVERT_Z_DIR false
 
 // @section extruder
 
